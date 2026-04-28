b8498aba2edd3a9e1acbf98073c2dde21afc8873
Andres Espinosa <andespinosa18@gmail.com>
2026-04-28 11:34:30 -0500

    Cuadro de creciente más grande

--- 2d_shape_mvc/2d_shape_mvc/ec/edu/espe/vista/FrmCreciente.Designer.cs
+++ 2d_shape_mvc/2d_shape_mvc/ec/edu/espe/vista/FrmCreciente.Designer.cs
@@ -45,109 +45,121 @@
             // panelDibujo
             // 
             this.panelDibujo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelDibujo.Location = new System.Drawing.Point(65, 195);
+            this.panelDibujo.Location = new System.Drawing.Point(80, 173);
+            this.panelDibujo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelDibujo.Name = "panelDibujo";
-            this.panelDibujo.Size = new System.Drawing.Size(455, 157);
+            this.panelDibujo.Size = new System.Drawing.Size(606, 308);
             this.panelDibujo.TabIndex = 23;
             // 
             // lblGrafico
             // 
             this.lblGrafico.AutoSize = true;
-            this.lblGrafico.Location = new System.Drawing.Point(37, 159);
+            this.lblGrafico.Location = new System.Drawing.Point(42, 153);
+            this.lblGrafico.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGrafico.Name = "lblGrafico";
-            this.lblGrafico.Size = new System.Drawing.Size(49, 15);
+            this.lblGrafico.Size = new System.Drawing.Size(53, 16);
             this.lblGrafico.TabIndex = 22;
             this.lblGrafico.Text = "Gráfico:";
             // 
             // lblArea
             // 
             this.lblArea.AutoSize = true;
-            this.lblArea.Location = new System.Drawing.Point(274, 86);
+            this.lblArea.Location = new System.Drawing.Point(365, 106);
+            this.lblArea.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblArea.Name = "lblArea";
-            this.lblArea.Size = new System.Drawing.Size(35, 15);
+            this.lblArea.Size = new System.Drawing.Size(39, 16);
             this.lblArea.TabIndex = 21;
             this.lblArea.Text = "Área:";
             // 
             // lblPerimetro
             // 
             this.lblPerimetro.AutoSize = true;
-            this.lblPerimetro.Location = new System.Drawing.Point(274, 56);
+            this.lblPerimetro.Location = new System.Drawing.Point(365, 69);
+            this.lblPerimetro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPerimetro.Name = "lblPerimetro";
-            this.lblPerimetro.Size = new System.Drawing.Size(64, 15);
+            this.lblPerimetro.Size = new System.Drawing.Size(68, 16);
             this.lblPerimetro.TabIndex = 20;
             this.lblPerimetro.Text = "Perímetro:";
             // 
             // txtArea
             // 
-            this.txtArea.Location = new System.Drawing.Point(344, 86);
+            this.txtArea.Location = new System.Drawing.Point(459, 106);
+            this.txtArea.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtArea.Name = "txtArea";
-            this.txtArea.Size = new System.Drawing.Size(64, 20);
+            this.txtArea.Size = new System.Drawing.Size(84, 22);
             this.txtArea.TabIndex = 19;
             // 
             // txtPerimetro
             // 
-            this.txtPerimetro.Location = new System.Drawing.Point(344, 56);
+            this.txtPerimetro.Location = new System.Drawing.Point(459, 69);
+            this.txtPerimetro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPerimetro.Name = "txtPerimetro";
-            this.txtPerimetro.Size = new System.Drawing.Size(64, 20);
+            this.txtPerimetro.Size = new System.Drawing.Size(84, 22);
             this.txtPerimetro.TabIndex = 18;
             // 
             // lblEntradas
             // 
             this.lblEntradas.AutoSize = true;
-            this.lblEntradas.Location = new System.Drawing.Point(23, 27);
+            this.lblEntradas.Location = new System.Drawing.Point(31, 33);
+            this.lblEntradas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEntradas.Name = "lblEntradas";
-            this.lblEntradas.Size = new System.Drawing.Size(59, 15);
+            this.lblEntradas.Size = new System.Drawing.Size(64, 16);
             this.lblEntradas.TabIndex = 17;
             this.lblEntradas.Text = "Entradas:";
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(435, 68);
+            this.btnSalir.Location = new System.Drawing.Point(580, 84);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(100, 28);
             this.btnSalir.TabIndex = 16;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // btnRestaurar
             // 
-            this.btnRestaurar.Location = new System.Drawing.Point(174, 86);
+            this.btnRestaurar.Location = new System.Drawing.Point(232, 106);
+            this.btnRestaurar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRestaurar.Name = "btnRestaurar";
-            this.btnRestaurar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestaurar.Size = new System.Drawing.Size(100, 28);
             this.btnRestaurar.TabIndex = 15;
             this.btnRestaurar.Text = "Restaurar";
             this.btnRestaurar.UseVisualStyleBackColor = true;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(174, 50);
+            this.btnCalcular.Location = new System.Drawing.Point(232, 62);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(100, 28);
             this.btnCalcular.TabIndex = 14;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
             // 
             // txtRadio
             // 
-            this.txtRadio.Location = new System.Drawing.Point(92, 53);
+            this.txtRadio.Location = new System.Drawing.Point(123, 65);
+            this.txtRadio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtRadio.Name = "txtRadio";
-            this.txtRadio.Size = new System.Drawing.Size(57, 20);
+            this.txtRadio.Size = new System.Drawing.Size(75, 22);
             this.txtRadio.TabIndex = 13;
             // 
             // lblRadio
             // 
             this.lblRadio.AutoSize = true;
-            this.lblRadio.Location = new System.Drawing.Point(43, 53);
+            this.lblRadio.Location = new System.Drawing.Point(57, 65);
+            this.lblRadio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRadio.Name = "lblRadio";
-            this.lblRadio.Size = new System.Drawing.Size(43, 15);
+            this.lblRadio.Size = new System.Drawing.Size(47, 16);
             this.lblRadio.TabIndex = 12;
             this.lblRadio.Text = "Radio:";
             // 
             // FrmCreciente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 419);
+            this.ClientSize = new System.Drawing.Size(749, 516);
             this.Controls.Add(this.panelDibujo);
             this.Controls.Add(this.lblGrafico);
             this.Controls.Add(this.lblArea);
@@ -160,6 +172,7 @@
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.txtRadio);
             this.Controls.Add(this.lblRadio);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmCreciente";
             this.Text = "FrmCreciente";
             this.ResumeLayout(false);
